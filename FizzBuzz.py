dc134a67d83a83eeb026639a010607a335235cac
adob360 <alex.dobler@hotmail.de>
2020-03-19 19:07:39 +0100

    Added the main pgrm FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+new_list = []
+
+for i in range (1, 101):
+    if i%3 == 0 and i%5 == 0:
+        new_list.append("FizzBuzz")
+    elif i%3 == 0:
+        new_list.append ("Fizz")
+        
+    elif i%5== 0:
+        new_list.append ("Buzz")
+    else:
+        new_list.append (i)
+        
+print (new_list)
